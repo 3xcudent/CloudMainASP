68eb976b4ab31b07f1b7c959d8e42e426017b18a
3xcudent <excudent>
2022-01-17 00:14:32 +0100

    Updated AccountController

--- CloudMainASP/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ CloudMainASP/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -63,7 +63,7 @@ namespace CloudMainASP.Areas.Identity.Pages.Account
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
 
-            returnUrl ??= Url.Content("~/Z");
+            returnUrl ??= Url.Content("~/");
 
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
---
+++ CloudMainASP/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using CloudMainASP.Data;
+using CloudMainASP.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace CloudMainASP.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login(ApplicationUser user)
+        {
+            return View("~/Index");
+        }
+        public string GetProfilePicture(ApplicationUser user)
+        {
+            string picture = user.ProfilePicture;
+            return picture;
+         }
+    }
+}
